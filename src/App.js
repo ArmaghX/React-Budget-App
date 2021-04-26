314c764c1687b1361087678f06e73116f8bd7d59
ARMAGH <alv.gomez@outlook.com>
2021-04-26 11:00:20 +0200

    Create Income/Expense List Component. Update Styles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Header from './components/Header';
 import Balance from './components/Balance';
 import AddTransaction from './components/AddTransaction';
+import IncomeList from './components/IncomeList';
+import ExpenseList from './components/ExpenseList';
 
 const App = () => {
     return (
@@ -12,6 +14,8 @@ const App = () => {
                 <Header />
                 <Balance />
                 <AddTransaction />
+                <IncomeList />
+                <ExpenseList />
             </div>
         </div>
     )
