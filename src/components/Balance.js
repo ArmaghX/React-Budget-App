f8f170d3b8ee116cfaa45498b379b832f0188e4f
ARMAGH <alv.gomez@outlook.com>
2021-04-27 18:52:39 +0200

    Currency Mod

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -18,15 +18,15 @@ const Balance = () => {
     return (
         <div className="balance">
             <h2>Your Balance</h2>
-            <h3>${(totalIncome - totalExpense).toFixed(2)}</h3>
+            <h3>€{(totalIncome - totalExpense).toFixed(2)}</h3>
             <div className="income-expense">
                 <div className="plus">
                     <h3>Income</h3>
-                    <p>+${totalIncome}</p>
+                    <p>+€{totalIncome}</p>
                 </div>
                 <div className="minus">
                     <h3>Expenses</h3>
-                    <p>-${totalExpense}</p>
+                    <p>-€{totalExpense}</p>
                 </div>
             </div>
         </div>
