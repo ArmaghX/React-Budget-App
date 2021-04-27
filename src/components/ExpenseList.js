623446cb4b3521d42511bbab5126674978c7fbc1
ARMAGH <alv.gomez@outlook.com>
2021-04-27 17:20:46 +0200

    Create State Reducer for Global Context Supply

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,6 +4,7 @@ import ExpenseTransaction from './ExpenseTransaction';
 
 const ExpenseList = () => {
     const {expenseTransactions} = useContext(GlobalContext);
+    console.log(expenseTransactions);
     return (
         <div className="transactions transactions-expense">
             <h2>Transaction History</h2>
